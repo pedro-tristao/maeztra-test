b6dc6b673909adf35b07f22502f6d1b01c143066
Pedro Tristão <pedro.henrique_tristao@hotmail.com>
2023-12-27 18:16:06 -0300

    partners final

---
+++ layout/src/js/partners.js
@@ -0,0 +1,14 @@
+$('.partner--container--slick-mob').slick({
+    infinite: false,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '30px',
+    slidesToShow: 1,
+    mobileFirst: true,
+    responsive: [
+        {
+           breakpoint: 1050,
+           settings: "unslick"
+        }
+    ]
+  });
